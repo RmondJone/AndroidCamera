7b6f23571115822ad24aa8100ef68d3ba21c4306
guohanlin <guohanlin@xyz.cn>
2020-08-10 15:01:29 +0800

    [Bug修复](master): Android8.0以下预览黑屏修复

--- Camera/src/main/java/com/rmondjone/camera/CameraActivity.java
+++ Camera/src/main/java/com/rmondjone/camera/CameraActivity.java
@@ -5,9 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.hardware.Camera;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -122,7 +120,6 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camre_layout);
-        fitComprehensiveScreen();
         mMongolianLayerType = (MongolianLayerType) getIntent().getSerializableExtra("MongolianLayerType");
         initView();
         setOnclickListener();
@@ -136,22 +133,6 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         mOverCameraView = new OverCameraView(this);
         mPreviewLayout.addView(preview);
         mPreviewLayout.addView(mOverCameraView);
-    }
-
-    /**
-     * 作者：郭翰林
-     * 时间：2018/7/9 0009 8:54
-     * 注释：适配全面屏
-     */
-    private void fitComprehensiveScreen() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().addFlags(View.SYSTEM_UI_FLAG_FULLSCREEN);
-            getWindow().addFlags(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-            getWindow().addFlags(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-            getWindow().addFlags(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            getWindow().setNavigationBarColor(Color.TRANSPARENT);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }
     }
 
     /**
